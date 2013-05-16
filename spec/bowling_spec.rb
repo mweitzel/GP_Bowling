f8fabff15ef8ac8effc9e94d820b146f30971a5b
Matthew Weitzel <mweitzel@gmail.com>
2013-05-16 14:54:49 -0400

    threw in some penders.. how to structure this next bit

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -303,6 +303,15 @@ describe Bowling do
 
 	context "Evaluator" do
 		pending "get some daannnng scooooooooorrreeesss!!!!!!"
+=begin
+		(assert (pins 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0) 0)
+		(assert (pins 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1) 20)
+		(assert (pins 5 5 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0) 16)
+		(assert (pins 10 3 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0) 24)
+		(assert (pins 10 10 10 10 10 10 10 10 10 10 10 10) 300)
+=end
+		pending "stores by answer, and accrues data from how it performs agains its different specs"
+		#perhaps the evaluator does some sort of grouping
 	end
 
 	context "Picker" do
